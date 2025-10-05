91c231aca68cf428417905ac7242bb954fbfaefb
WataXScript <dalvo9216@gmail.com>
2025-10-05 20:58:48 +0800

    Update main.lua

--- Loader/main.lua
+++ Loader/main.lua
@@ -4,7 +4,7 @@ local StarterGui = game:GetService("StarterGui")
 local player = Players.LocalPlayer
 
 local urlVip = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/vip.txt"
-local urlSatuan = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/antartika.txt"
+local urlSatuan = "https://raw.githubusercontent.com/WataXScript/VerifikasiScWata/refs/heads/main/Loader/14.txt"
 
 local successUrls = {
     "https://raw.githubusercontent.com/WataXScript/WataXMountAtin/main/Loader/WataX.lua",
