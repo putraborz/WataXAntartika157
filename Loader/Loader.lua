661f3ef38f6e7b4e847534a5fc48f26ca4da25cd
putraborz <putra.myapi@gmail.com>
2025-10-30 13:10:00 +0700

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/WataXScript/WataXAntartika157/main/Loader/main.lua"
+local url = "https://raw.githubusercontent.com/putraborz/WataXAntartika157/refs/heads/main/Loader/main.lua"
 local s, e = pcall(function()
     return loadstring(game:HttpGet(url))()
 end)
